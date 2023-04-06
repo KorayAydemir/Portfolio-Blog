a23609c5b971aefcaffd807654df4264f33b4131
Koray Aydemir <korayaydemir276@gmail.com>
2023-04-06 21:04:30 +0300

    add typewriter effects

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {},
+    screens: {
+      'm-l': '414px',
+      'm-s': '360px',
+      'sm': '640px',
+      'md': '768px',
+      'lg': '1024px',
+      'xl': '1280px',
+      '2xl': '1536px',
+    },
   },
+  darkMode: 'class',
   plugins: [],
 }
 
