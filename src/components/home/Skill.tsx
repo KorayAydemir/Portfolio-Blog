86e848590bc6ec01dcbf05c633d403199800e106
Koray Aydemir <korayaydemir276@gmail.com>
2023-04-10 16:14:17 +0300

    add contact form

--- src/components/home/Skill.tsx
+++ src/components/home/Skill.tsx
@@ -7,7 +7,7 @@ export function Skill(props: Props) {
   const skill = props.fields.map((field) => {
     const { name, Icon } = field;
     return (
-      <div className="w-6/12 flex justify-center">
+      <div key={name} className="w-6/12 flex justify-center">
         <div className="flex font-bold w-32 mb-2">
           <Icon className="block my-auto mr-2 text-2xl" />
           <span className="text-lg">{name}</span>
