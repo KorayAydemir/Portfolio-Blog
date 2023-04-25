096ba1511ba52c9b7a802c91336ddabf69994c2c
Koray Aydemir <korayaydemir276@gmail.com>
2023-04-25 16:18:01 +0300

    test

--- src/pages/api/revalidate.tsx
+++ src/pages/api/revalidate.tsx
@@ -13,6 +13,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse<
     return res.status(401).json({ message: "Must be a POST request" })
   }
 
+  console.log(req.headers['x-sanity-webhook-signature'])
 
   if (!isValidRequest(req, secret)) {
     res.status(401).json({ message: `Invalid signature: ${secret}`, })
