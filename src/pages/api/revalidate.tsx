471f9b9f332791d292ce8d2fe676f6d4a7caa2b8
Koray Aydemir <korayaydemir276@gmail.com>
2023-04-25 15:24:24 +0300

    revalidate sanity webhook

---
+++ src/pages/api/revalidate.tsx
@@ -0,0 +1,37 @@
+import { isValidRequest } from "@sanity/webhook"
+import type { NextApiRequest, NextApiResponse } from "next"
+
+type Data = {
+  message: string
+}
+
+const secret = process.env.SANITY_ISR_WEBHOOK!
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse<Data>) {
+  if (req.method !== "POST") {
+    console.error("Must be a POST request")
+    return res.status(401).json({ message: "Must be a POST request" })
+  }
+
+  if (!isValidRequest(req, secret)) {
+    res.status(401).json({ message: "Invalid signature" })
+    return
+  }
+
+  try {
+    const {
+      body: { email, postSlug, approved },
+    } = req
+
+    switch (approved) {
+      case true:
+        await res.revalidate(`/blog/${postSlug}`)
+        return res.json({ message: `Revalidated "${email}"'s comment on slug "${postSlug}"` })
+    }
+
+    return res.json({ message: "No managed type" })
+  } catch (err) {
+    return res.status(500).send({ message: "Error revalidating" })
+  }
+}
+
