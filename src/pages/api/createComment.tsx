16b621485d5faa3af2e5a81c0fab5eb2caa89c11
Koray Aydemir <korayaydemir276@gmail.com>
2023-05-08 00:22:51 +0300

    Recaptcha test

--- src/pages/api/createComment.tsx
+++ src/pages/api/createComment.tsx
@@ -1,22 +1,48 @@
 // This Next.js template already is configured to write with this Sanity Client
 import { previewClient } from 'lib/sanity.tsx'
 
+const verifyRecaptcha = async (token: any) => {
+    const secretKey = process.env.RECAPTHA_SECRET_KEY;
+
+    let verificationUrl = "https://www.google.com/recaptcha/api/siteverify?secret=" +
+        secretKey +
+        "&response=" +
+        token;
+
+    return await fetch(verificationUrl, { method: 'POST' })
+        .then(response => response.json())
+        .then(data => { return data })
+        .catch(error => { throw error })
+}
+
 export default async function createComment(req: any, res: any) {
     // Destructure the pieces of our request
-    const { register, name, email, comment } = JSON.parse(req.body)
+    const { register, name, email, comment, token } = JSON.parse(req.body)
     try {
-        // Use our Client to create a new document in Sanity with an object  
-        await previewClient.create({
-            _type: 'comment',
-            post: register,
-            name,
-            email,
-            comment
-        })
+        const response = await verifyRecaptcha(token);
+        console.log(response)
+        if (response.data.success && response.data.score >= 0.5) {
+            // Use our Client to create a new document in Sanity with an object  
+            await previewClient.create({
+                _type: 'comment',
+                post: register,
+                name,
+                email,
+                comment
+            })
+
+            return res.status(200).json({ message: 'Comment submitted' })
+        } else {
+            return res.json({
+                status: "failed",
+                message: "Something went wrong, please try again."
+            })
+        }
     } catch (err) {
+        const response = await verifyRecaptcha(token);
         console.error(err)
-        return res.status(500).json({ message: `Couldn't submit comment`, err })
+        return res.status(500).json({ message: `Couldn't submit comment`, response })
     }
 
-    return res.status(200).json({ message: 'Comment submitted' })
+    //return res.status(200).json({ message: 'Comment submitted' })
 }
