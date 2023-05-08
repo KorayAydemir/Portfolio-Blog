e32638fae74ca782fe22007bec9c2f29f6cdda15
Koray Aydemir <korayaydemir276@gmail.com>
2023-05-08 20:47:14 +0300

    Update base colors and recaptcha

--- src/components/blog/CommentForm.tsx
+++ src/components/blog/CommentForm.tsx
@@ -44,7 +44,6 @@ export const CommentForm = ({ _id }: any) => {
 
         try {
             const token = await executeRecaptcha();
-            console.log(token)
             if (!token) { console.log("failed to send"); return; }
             await fetch('/api/createComment', {
                 method: 'POST',
@@ -56,6 +55,13 @@ export const CommentForm = ({ _id }: any) => {
             setFormData(err)
         }
     }
+
+    const recaptchaText = (
+        <div className="text-gray-500 text-sm mb-5">This site is protected by reCAPTCHA and the Google
+            <a href="https://policies.google.com/privacy"> Privacy Policy</a> and
+            <a href="https://policies.google.com/terms"> Terms of Service</a > apply.
+        </div>
+    )
 
     return (
         <>
@@ -69,38 +75,39 @@ export const CommentForm = ({ _id }: any) => {
                 <input {...register("register")} type="hidden" name="post" value={_id} />
 
                 <div className="block mb-5">
-                    <label htmlFor="name" className="text-gray-700">Name</label>
+                    <label htmlFor="name" className="text-gray-700 dark:text-gray-300">Name</label>
 
                     <input {...register("name", { required: true, maxLength: 60 })}
                         name="name" placeholder="John Cena"
                         aria-invalid={errors.name ? "true" : "false"}
-                        className="form-input mt-1 block w-full" />
+                        className="form-input mt-1 block w-full p-1" />
                     {errors.name?.type === "required" && <p className="text-red-400" role="alert">Name can&apos;t be empty.</p>}
                     {errors.name?.type === "maxLength" && <p className="text-red-400" role="alert">Name can&apos;t be longer than 60 characters.</p>}
                 </div>
 
                 <div className="block mb-5">
-                    <label htmlFor="email" className="text-gray-700">Email</label>
-                    <input  {...register("email", { pattern: /^\S+@\S+$/i, required: true, maxLength: 254, minLength: 5 })}
-                        name="email" type="email" className="form-input mt-1 block w-full" placeholder="your@email.com" />
+                    <label htmlFor="email" className="text-gray-700 dark:text-gray-300">Email</label>
+                    <input {...register("email", { pattern: /^\S+@\S+$/i, required: true, maxLength: 254, minLength: 5 })}
+                        name="email" type="email" className="form-input mt-1 block w-full p-1" placeholder="your@email.com" />
                     {errors.email?.type === "required" && <p className="text-red-400" role="alert">Email can&apos;t be empty.</p>}
                     {errors.email?.type === "maxLength" && <p className="text-red-400" role="alert">Email can&apos;t be longer than 254 characters.</p>}
                     {errors.email?.type === "minLength" && <p className="text-red-400" role="alert">Email can&apos;t be shorter than 5 characters.</p>}
                     {errors.email?.type === "pattern" && <p className="text-red-400" role="alert">Email is invalid.</p>}
                 </div>
 
-                <div className="block mb-5">
-                    <label htmlFor="comment" className="text-gray-700">Comment</label>
+                <div className="block mb-2">
+                    <label htmlFor="comment" className="text-gray-700 dark:text-gray-300">Comment</label>
                     <textarea  {...register("comment", { required: true, maxLength: 1024 })}
                         name="comment" placeholder="Enter your comment here."
-                        className="form-textarea mt-1 block w-full" rows={8}
+                        className="form-textarea mt-1 block w-full p-1" rows={8}
                         aria-invalid={errors.comment ? "true" : "false"}
                     ></textarea>
                     {errors.comment?.type === "required" && <p className="text-red-400" role="alert">Comment can&apos;t be empty.</p>}
                     {errors.comment?.type === "maxLength" && <p className="text-red-400" role="alert">Comment can&apos;t be longer than 1024 characters.</p>}
                 </div>
 
-                <input type="submit" className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" />
+                {recaptchaText}
+                <input value="Submit Comment" type="submit" className="shadow bg-purple-500 hover:bg-purple-400 focus:shadow-outline focus:outline-none text-white font-bold py-2 px-4 rounded" />
             </form >
         </>
     )
