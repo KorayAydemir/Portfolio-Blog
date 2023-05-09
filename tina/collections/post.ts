c941c2aab2ec5037a4c3f2335f87681c5d8a1c21
Koray Aydemir <korayaydemir276@gmail.com>
2023-05-09 23:10:04 +0300

    refactored everything except src

---
+++ tina/collections/post.ts
@@ -0,0 +1,51 @@
+import { Document } from "tina/__generated__/types"
+import { Collection } from "tinacms"
+
+export const Post: Collection = {
+    name: "post",
+    label: "Posts",
+    path: "content/posts",
+    fields: [
+        {
+            type: "string",
+            name: "title",
+            label: "Title",
+            isTitle: true,
+            required: true,
+        },
+
+        {
+            type: "rich-text",
+            name: "body",
+            label: "Body",
+            isBody: true,
+            required: true,
+        },
+        {
+            type: "datetime",
+            name: "date",
+            label: "Date",
+            isTitle: true,
+            required: true,
+        },
+        {
+            label: 'Tags',
+            name: 'tags',
+            type: 'string',
+            list: true,
+            required: true,
+        },
+
+        {
+            type: "string",
+            name: "summary",
+            label: "Summary",
+            required: true,
+        },
+    ],
+    ui: {
+        // This is an DEMO router. You can remove this to fit your site
+        router: ({ document }) => `/blog/${document._sys?.filename}`,
+    },
+}
+
--- tina/config.ts
+++ tina/config.ts
@@ -1,72 +1,25 @@
 import { defineConfig } from "tinacms";
+import { Post } from "./collections/post";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
 
 export default defineConfig({
-  branch,
-  clientId: "086c7210-2047-4bfd-9f6a-067b9622a0e8", // Get this from tina.io
-  token: "8ddf24c51a3bf8095aeccd9051cdc72b1b21b65b", // Get this from tina.io
+    branch,
+    clientId: "086c7210-2047-4bfd-9f6a-067b9622a0e8", // Get this from tina.io
+    token: "8ddf24c51a3bf8095aeccd9051cdc72b1b21b65b", // Get this from tina.io
 
-  build: {
-    outputFolder: "admin",
-    publicFolder: "public",
-  },
-  media: {
-    tina: {
-      mediaRoot: "uploads",
-      publicFolder: "public",
+    build: {
+        outputFolder: "admin",
+        publicFolder: "public",
     },
-  },
-  schema: {
-    collections: [
-      {
-        name: "post",
-        label: "Posts",
-        path: "content/posts",
-        fields: [
-          {
-            type: "string",
-            name: "title",
-            label: "Title",
-            isTitle: true,
-            required: true,
-          },
-
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
-            required: true,
-          },
-          {
-            type: "datetime",
-            name: "date",
-            label: "Date",
-            isTitle: true,
-            required: true,
-          },
-          {
-            label: 'Tags',
-            name: 'tags',
-            type: 'string',
-            list: true,
-            required: true,
-          },
-
-          {
-            type: "string",
-            name: "summary",
-            label: "Summary",
-            required: true,
-          },
-        ],
-        ui: {
-          // This is an DEMO router. You can remove this to fit your site
-          router: ({ document }) => `/blog/${document._sys.filename}`,
+    media: {
+        tina: {
+            mediaRoot: "uploads",
+            publicFolder: "public",
         },
-      },
-    ],
-  },
+    },
+    schema: {
+        collections: [Post],
+    },
 });
