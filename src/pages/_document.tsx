2ec2c199fbe08c197ab73ef17623c289927f9cf4
Koray Aydemir <korayaydemir276@gmail.com>
2023-05-09 19:56:27 +0300

    Update _document head for manifest.json

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,6 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
     return (
         <Html lang="en">
+            <link rel="manifest" href="/manifest.json" />
+            <link rel="apple-touch-icon" href="/favicon.ico"></link>
+            <meta name="theme-color" content="#fff" />
             <Head>
                 <noscript>
                     <link rel="stylesheet" type="text/css" href="/noscript.css" />
