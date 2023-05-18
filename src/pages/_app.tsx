e992fb0f799654aa6783d89251d0d05197efa204
Koray Aydemir <korayaydemir276@gmail.com>
2023-05-18 20:59:13 +0300

    add mobile responsive meta tags to _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,22 +1,31 @@
 import "@component/styles/globals.css";
 import { ThemeProvider } from "next-themes";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { GoogleReCaptchaProvider } from "react-google-recaptcha-v3";
 
 export default function App({ Component, pageProps }: AppProps) {
     return (
-        <GoogleReCaptchaProvider
-            reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTHA_SITE_KEY!}
-            scriptProps={{
-                async: false, // optional, default to false,
-                defer: true, // optional, default to false
-                appendTo: "body", // optional, default to "head", can be "head" or "body",
-                nonce: undefined,
-            }}
-        >
-            <ThemeProvider enableSystem={true} attribute="class">
-                <Component {...pageProps} />
-            </ThemeProvider>
-        </GoogleReCaptchaProvider>
+        <>
+            <Head>
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1.0"
+                />
+            </Head>
+            <GoogleReCaptchaProvider
+                reCaptchaKey={process.env.NEXT_PUBLIC_RECAPTHA_SITE_KEY!}
+                scriptProps={{
+                    async: false, // optional, default to false,
+                    defer: true, // optional, default to false
+                    appendTo: "body", // optional, default to "head", can be "head" or "body",
+                    nonce: undefined,
+                }}
+            >
+                <ThemeProvider enableSystem={true} attribute="class">
+                    <Component {...pageProps} />
+                </ThemeProvider>
+            </GoogleReCaptchaProvider>
+        </>
     );
 }
