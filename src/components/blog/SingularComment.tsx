9b904a69ba7dddafd29ace5abbfb152f44fc454e
Koray Aydemir <korayaydemir276@gmail.com>
2023-05-18 20:08:15 +0300

    styling of comments

--- src/components/blog/SingularComment.tsx
+++ src/components/blog/SingularComment.tsx
@@ -1,4 +1,5 @@
 import { ReplyForm } from "./ReplyForm";
+import { createContext, useContext } from "react";
 
 interface SingularComment {
     isActive: boolean;
@@ -12,6 +13,8 @@ interface SingularComment {
     postId: string;
 }
 
+export const DepthContext = createContext<number>(0);
+
 export const SingularComment = ({
     _id,
     _createdAt,
@@ -23,23 +26,27 @@ export const SingularComment = ({
     activeReplyId,
     postId,
 }: SingularComment) => {
+    const parentDepth = useContext(DepthContext);
+
     const renderReplies = replies ? (
-        <ul className="ml-10">
-            {replies.map((c: SingularComment) => (
-                <SingularComment
-                    postId={postId}
-                    key={c._id}
-                    replies={c.replies}
-                    _id={c._id}
-                    _createdAt={c._createdAt}
-                    name={c.name}
-                    comment={c.comment}
-                    isActive={activeReplyId === c._id}
-                    toggleReplyForm={toggleReplyForm}
-                    activeReplyId={activeReplyId}
-                />
-            ))}
-        </ul>
+        <DepthContext.Provider value={parentDepth + 1}>
+            <ul className="ml-10">
+                {replies.map((c: SingularComment) => (
+                    <SingularComment
+                        postId={postId}
+                        key={c._id}
+                        replies={c.replies}
+                        _id={c._id}
+                        _createdAt={c._createdAt}
+                        name={c.name}
+                        comment={c.comment}
+                        isActive={activeReplyId === c._id}
+                        toggleReplyForm={toggleReplyForm}
+                        activeReplyId={activeReplyId}
+                    />
+                ))}
+            </ul>
+        </DepthContext.Provider>
     ) : (
         false
     );
@@ -52,18 +59,21 @@ export const SingularComment = ({
                 <time dateTime={_createdAt}>{_createdAt.split("T")[0]}</time>
             </h4>
             <p>{comment}</p>
-            <div className="flex">
-                <button
-                    className="ml-auto mr-5 mt-4 rounded-md border-2 border-black px-2 py-1
+
+            {parentDepth < 3 && (
+                <div className="flex">
+                    <button
+                        className="ml-auto mr-5 mt-2 rounded-md border-2 border-black px-2 py-1
                             hover:bg-black hover:text-white dark:border-white dark:hover:bg-white
                             dark:hover:text-black"
-                    onClick={() => toggleReplyForm(_id)}
-                >
-                    <span className="font-bold">Reply</span>
-                </button>
-                {isActive && <ReplyForm _id={_id} postId={postId} />}
-            </div>
-            <hr className="mb-5 mt-5 border-black dark:border-white" />
+                        onClick={() => toggleReplyForm(_id)}
+                    >
+                        <span className="font-bold">Reply</span>
+                    </button>
+                    {isActive && <ReplyForm _id={_id} postId={postId} />}
+                </div>
+            )}
+            <hr className="mb-3 mt-3 border-black dark:border-white" />
             {renderReplies}
         </li>
     );
